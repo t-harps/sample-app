8884ef6d7bb9c67af0a0d77a440438091d50aee1
t-harps <t.w.harps@gmail.com>
2015-08-19 12:02:36 +0100

    Finish log in/log out

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -59,8 +59,12 @@ class UserTest < ActiveSupport::TestCase
   	assert_not @user.valid?
   end
 
-    test "password should have a minimum length" do
+  test "password should have a minimum length" do
   	@user.password = @user.password_confirmation = "a" * 5
   	assert_not @user.valid?
+  end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
   end
 end
